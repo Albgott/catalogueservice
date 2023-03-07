0e14a3430da69671ad386c6f6db5563e939a31cf
Alberto Morán <a.moran.cuenca@gmail.com>
2023-03-07 23:44:23 +0100

    fix: dowload big images

--- src/main/java/com/albgott/catalogueservice/file/adapter/REST/FileDownloadController.java
+++ src/main/java/com/albgott/catalogueservice/file/adapter/REST/FileDownloadController.java
@@ -6,12 +6,14 @@ import com.albgott.catalogueservice.file.application.download.DownloadFileServic
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@Transactional(readOnly=true)
 @RestController
 public class FileDownloadController {
     private final DownloadFileService downloadFileService;
